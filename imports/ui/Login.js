a64d38ddf1a6719c04186039ca5df9811d9f599a
Felix Lucas <felixlucas38@gmail.com>
2018-02-17 14:45:58 -0300

    Add readme docs

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -43,7 +43,7 @@ export default class Login extends React.Component {
         return (
             <div className="boxed-view">
                 <div className="boxed-view__box">
-                    <h1>Short Lnk</h1>
+                    <h1>Short Lnk App</h1>
                     {/* <p>Login to your account</p> */}
 
                     {/* <br /> */}
